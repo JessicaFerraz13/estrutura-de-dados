0ebf877f3db9b435b8b1a8fd7a8803547282af3c
Jéssica Ferraz <106453893+JessicaFerraz13@users.noreply.github.com>
2023-08-16 22:09:12 -0300

    Update TabelaHash.py

--- Tabela Hash/TabelaHash.py
+++ Tabela Hash/TabelaHash.py
@@ -1,8 +1,8 @@
 from ListasEncadeadas import *
 
 class Tabela:
-    def __init__(self, Max = 7):
-        self.vetor = [None] * (Max * 2 + 1)
+    def __init__(self, Max = 29):
+        self.vetor = [None] * (Max + 1)
         self.tamanho = 0
 
     def imprimir(self): # Apenas para visualizar se esta funcionando corretamente
@@ -14,18 +14,18 @@ class Tabela:
                 print('\n')
 
     def inserir(self, chave, info):
-        posicao = (chave % 14) + 1
+        posicao = (chave % Max) + 1
         if self.vetor[posicao] == None:
             self.vetor[posicao] = ListaEncad()
         self.vetor[posicao].insere(chave, info)
 
     def consultar(self, chave):
-        posicao = (chave % 14) + 1
+        posicao = (chave % Max) + 1
         if self.vetor[posicao] == None:
             return False
         return self.vetor[posicao].busca(chave)
 
     def excluir(self, chave):
-        posicao = (chave % 14) + 1
+        posicao = (chave % Max) + 1
         if self.vetor[posicao] != None:
             self.vetor[posicao].exclui(chave)
